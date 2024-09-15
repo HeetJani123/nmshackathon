12b2c57b9813c14c5b8c0b39cf5dfac5d18eb59d
Trishagrawal <147096107+Trishagrawal@users.noreply.github.com>
2024-09-15 10:57:49 +0400

    Update HScene.js

--- src/components/HScene.js
+++ src/components/HScene.js
@@ -13,7 +13,7 @@ function Scene() {
     camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, mouse.x * -Math.PI * 0.25, 0.01)  })
 
   return (
-    <ScrollControls pages={9}>
+    <ScrollControls pages={10}>
       <Scroll>
         <Particles pages={9} size={14994}/>
       </Scroll>
